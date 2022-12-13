b64e2dcbdb44c73040e229f6a51ed60a7589bd8b
Vlad <jeywer2747@gmail.com>
2022-12-13 06:06:56 +0300

    pics

--- kaspagescript.js
+++ kaspagescript.js
@@ -1,11 +1,12 @@
 let priceVisability = false;
 let priceDisplay = "none";
-let animationDirection;
+let animationName;
 
 let priceSectionOn = function(){
     priceVisability = !priceVisability;
     !priceVisability ? priceDisplay = "none" : priceDisplay = "block";
-    priceVisability ? animationDirection = "Normal" : animationDirection = "Reverse";
+    priceVisability ? animationName = "1" : animationName = "0";
     pricelist.style.display = priceDisplay;
-    arrow.style.animation = `arrowRotate${animationDirection} 0.25s linear forwards `;
-}
+    flower.style.animation = `flowerRolling${animationName} 0.25s linear forward `;
+    
+    }

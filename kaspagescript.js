69f9fa4046d75198a307d7c51fcea448482bbfcd
Vlad <jeywer2747@gmail.com>
2022-12-17 14:27:16 +0300

    some edits

--- kaspagescript.js
+++ kaspagescript.js
@@ -5,8 +5,7 @@ let animationName;
 let priceSectionOn = function(){
     priceVisability = !priceVisability;
     !priceVisability ? priceDisplay = "none" : priceDisplay = "block";
-    priceVisability ? animationName = "1" : animationName = "0";
+    priceVisability ? animationName = "Normal" : animationName = "Reverse";
     pricelist.style.display = priceDisplay;
-    flower.style.animation = `flowerRolling${animationName} 0.25s linear forward `;
-    
+    flower.style.animation = `flowerRolling${animationName} 0.5s linear 1 forwards`;
     }
